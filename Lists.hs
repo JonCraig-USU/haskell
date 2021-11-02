8398769d1ef2867050598b26652c3632f7c1922b
JonCraig <jonathan.craig@aggiemail.usu.edu>
2021-11-01 22:10:18 -0600

    earlier push forgot to save. lel

--- Lists.hs
+++ Lists.hs
@@ -1,44 +1,58 @@
-module Lists where
+module Main where
 
-  -- I've left this sample definition in here, you should delete it
-  oddNumbers :: Int -> [Int]
-  oddNumbers n = [ x | x <- [1..n], (mod x 1) == 0] 
+main :: IO ()
+main = print $ (take 10 (multiplesOfNumbers 2))
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
 
 -- Counting Numbers
-countingNumbers :: Num n => [n]
+countingNumbers = [1..] 
 
 
 -- multiplesOfNumbers
-multiplesOfNumbers
+multiplesOfNumbers n = map (* n) [1..] 
 
 
--- woodallNumbers
-woodallNumbers
+-- -- woodallNumbers
+woodallNumbers = map (\n -> n * 2^n - 1) [1..]
 
 
--- padovanNumbers
-padovanNumbers
+-- -- padovanNumbers
+pado 0 = 1
+pado 1 = 1
+pado 2 = 1
+pado n = pado (n - 2) + pado (n - 3)
+
+padovanNumbers = map pado [0..]
 
 
--- order
-order
+-- -- order
+order :: Ord a => [a] -> [a] -> [a]
+order [] n = n
+order n [] = n
+order (x:xs) (y:ys) | y < x     = y : order (x:xs) ys
+order (x:xs) (y:ys) | otherwise = x : order xs (y:ys)
 
 
--- pairUp
-pairUp
+-- -- pairUp
+pairUp :: Ord a => [a] -> [[a]]
+pairUp [] = []
+pairUp (x:y:xs) = [x, y] : pairUp xs
+pairUp (x:xs) = [x] : pairUp xs
 
 
+-- -- runLengthEncoding
 -- runLengthEncoding
-runLengthEncoding
 
 
+-- -- listPairApply
 -- listPairApply
-listPairApply
 
 
+-- -- composeList
 -- composeList
-composeList
-
-
 
 
--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,8 @@
 module Main where
 
-
-
 main :: IO ()
 main = print $ (take 10 (multiplesOfNumbers 2))
 
--- main :: factorial 5
--- main = putStrLn "Hello, Haskell!"
 factorial :: Int -> Int
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
@@ -21,19 +17,31 @@ multiplesOfNumbers n = map (* n) [1..]
 
 
 -- -- woodallNumbers
--- woodallNumbers
+woodallNumbers = map (\n -> n * 2^n - 1) [1..]
 
 
 -- -- padovanNumbers
--- padovanNumbers
+pado 0 = 1
+pado 1 = 1
+pado 2 = 1
+pado n = pado (n - 2) + pado (n - 3)
+
+padovanNumbers = map pado [0..]
 
 
 -- -- order
--- order
+order :: Ord a => [a] -> [a] -> [a]
+order [] n = n
+order n [] = n
+order (x:xs) (y:ys) | y < x     = y : order (x:xs) ys
+order (x:xs) (y:ys) | otherwise = x : order xs (y:ys)
 
 
 -- -- pairUp
--- pairUp
+pairUp :: Ord a => [a] -> [[a]]
+pairUp [] = []
+pairUp (x:y:xs) = [x, y] : pairUp xs
+pairUp (x:xs) = [x] : pairUp xs
 
 
 -- -- runLengthEncoding
